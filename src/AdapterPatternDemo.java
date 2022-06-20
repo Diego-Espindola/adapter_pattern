6010c2d76594b58e016f97cdd9898737d09e9771
diego-espindola <diegoespindola9404@gmail.com>
2022-06-19 23:02:06 -0300

    Finished Principal

--- src/AdapterPatternDemo.java
+++ src/AdapterPatternDemo.java
@@ -1,5 +1,14 @@
+import com.adapter.AudioPlayer;
+
 public class AdapterPatternDemo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        AudioPlayer audioPlayer = new AudioPlayer();
+
+        audioPlayer.play("vlc", "Darandandan.vlc");
+        audioPlayer.play("mp4", "alone.mp4");
+        audioPlayer.play("mp3", "musica para os meus ouvidos.mp3");
+        audioPlayer.play("gsm", "mind me.gsm");
+
     }
 }
